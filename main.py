dda5ebc3f987c2604f21f1b97ed3417e3c13187d
oscarrbridge <oscarbridge.nz@gmail.com>
2021-08-01 20:20:53 -1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from secret import *
+import discord
+import time
+
+client = discord.Client()
+
+TARGET_CHANNEL = 871292302482636826
+
+
+@client.event
+async def on_ready():
+    channel = client.get_channel(TARGET_CHANNEL)
+    await client.change_presence(status=discord.Status.online)
+    await channel.connect()
+    print('Logged in as {0.user}'.format(client))
+    while True:
+        async client.VoiceChannel.connect
+        """
+        print("top")
+        channel = client.get_channel(TARGET_CHANNEL)
+        usrs_list = {}
+        usrs_id = channel.voice_states
+        for usrs in usrs_id:
+            usrs_list[len(usrs_list) + 1] = usrs
+        if len(usrs_id) >= 2:
+            for voice in client.voice_clients:
+                await voice.disconnect()
+        print(usrs_list)
+        time.sleep(2)
+        """
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!test'):
+        await message.channel.send('running!')
+
+client.run(TOKEN)
+
